86727b53d28785ea7584e789f1b4c2e92aab4c60
Kaustav Biswas <kaustavbiswas1697@gmail.com>
2021-10-28 23:13:46 +0530

    added bitwiseShift

---
+++ bitwise op/bitShift.py
@@ -0,0 +1,16 @@
+'''
+Left shift and right shift with operators <<, >>:
+'''
+
+x = 9  # 0b1001
+
+print(x << 1)
+print(bin(x << 1))
+# 18
+# 0b10010
+
+print(x >> 1)
+print(bin(x >> 1))
+# 4
+# 0b100
+
