496b42ad433eaa1dd32bb80fd1508c1dce954d92
Kaustav Biswas <kaustavbiswas1697@gmail.com>
2021-10-28 23:25:57 +0530

    added bitwiseAND

---
+++ bitwise op/bitwiseAND.py
@@ -0,0 +1,12 @@
+'''
+Bitwise AND with the & operator:
+
+It also outputs the result of conversion to binary string with bin()
+'''
+x = 9   # 0b1001
+y = 10  # 0b1010
+
+print(x & y)
+print(bin(x & y))
+# 8
+# 0b1000
